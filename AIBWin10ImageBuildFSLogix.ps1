014c1edc0d172192309c485a1ea396d14702cc26
umarlatif-lab <78913169+umarlatif-lab@users.noreply.github.com>
2021-03-10 10:06:26 +0000

    Update AIBWin10ImageBuildFSLogix.ps1

--- AIBWin10ImageBuildFSLogix.ps1
+++ AIBWin10ImageBuildFSLogix.ps1
@@ -4,7 +4,7 @@
 New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
 
 #Install VSCode
-Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?Linkid=852157' -OutFile 'c:\temp\VScode.exe'
+Invoke-WebRequest -Uri 'https://az764295.vo.msecnd.net/stable/f30a9b73e8ffc278e71575118b6bf568f04587c8/VSCodeUserSetup-x64-1.54.1.exe' -OutFile 'c:\temp\VScode.exe'
 Invoke-Expression -Command 'c:\temp\VScode.exe /verysilent'
 
 #Start sleep
@@ -39,5 +39,6 @@ Invoke-WebRequest -Uri 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binar
 Invoke-Expression -Command 'c:\temp\websocket.msi /quiet'
 #Start sleep
 Start-Sleep -Seconds 10
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri 'https://statics.teams.cdn.office.net/production-windows-x64/1.3.00.4461/Teams_windows_x64.msi' -OutFile 'c:\temp\Teams.msi'
 Invoke-Expression -Command 'msiexec /i C:\temp\Teams.msi /quiet /l*v C:\temp\teamsinstall.log ALLUSER=1 ALLUSERS=1'

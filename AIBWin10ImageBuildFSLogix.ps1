30dad6baba091155074dcb4ca5ae98552e53e9f8
umarlatif-lab <78913169+umarlatif-lab@users.noreply.github.com>
2021-03-24 12:01:23 +0000

    Update AIBWin10ImageBuildFSLogix.ps1

--- AIBWin10ImageBuildFSLogix.ps1
+++ AIBWin10ImageBuildFSLogix.ps1
@@ -3,7 +3,7 @@
 write-host 'AIB Customization: Downloading FsLogix'
 New-Item -Path C:\\ -Name fslogix -ItemType Directory -ErrorAction SilentlyContinue
 $LocalPath = 'C:\\fslogix'
-$WVDflogixURL = 'https://raw.githubusercontent.com/DeanCefola/Azure-WVD/master/PowerShell/FSLogixSetup.ps1'
+$WVDflogixURL = 'https://github.com/umarlatif-lab/wvd/blob/main/FSLogixSetupParams.ps1'
 $WVDFslogixInstaller = 'FSLogixSetup.ps1'
 $outputPath = $LocalPath + '\' + $WVDFslogixInstaller
 Invoke-WebRequest -Uri $WVDflogixURL -OutFile $outputPath
